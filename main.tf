feb603cfe0e47fc6b4528f8a90b87430ada698c8
gtalarico <gtalarico.dev@gmail.com>
2020-05-26 00:28:15 -0700

    Adds Cloudfront

--- main.tf
+++ main.tf
@@ -39,3 +39,116 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
   bucket = aws_s3_bucket.bucket.id
   policy = data.template_file.bucketpolicy.rendered
 }
+
+
+
+// Cloudfront
+locals {
+  s3_origin_id = "aecworks_s3_origin"
+}
+
+
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.bucket.bucket_regional_domain_name
+    origin_id   = local.s3_origin_id
+
+    # s3_origin_config {
+    #   origin_access_identity = "origin-access-identity/cloudfront/ABCDEFG1234567"
+    # }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "Some comment"
+  # default_root_object = "index.html"
+
+  # logging_config {
+  #   include_cookies = false
+  #   bucket          = "mylogs.s3.amazonaws.com"
+  #   prefix          = "myprefix"
+  # }
+
+  aliases = ["static.aec.works"]
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  # Cache behavior with precedence 0
+  ordered_cache_behavior {
+    path_pattern     = "/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+      headers      = ["Origin"]
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    compress               = true
+    # min_ttl                = 0
+    # default_ttl            = 86400
+    # max_ttl                = 31536000
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  # Cache behavior with precedence 1
+  # ordered_cache_behavior {
+  #   path_pattern     = "/content/*"
+  #   allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+  #   cached_methods   = ["GET", "HEAD"]
+  #   target_origin_id = local.s3_origin_id
+
+  #   forwarded_values {
+  #     query_string = false
+
+  #     cookies {
+  #       forward = "none"
+  #     }
+  #   }
+
+  #   min_ttl                = 0
+  #   default_ttl            = 3600
+  #   max_ttl                = 86400
+  #   compress               = true
+  #   viewer_protocol_policy = "redirect-to-https"
+  # }
+
+  # price_class = "PriceClass_200"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  # tags = {
+  #   Environment = "production"
+  # }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
