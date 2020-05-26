828e948fae9000cac3f3a0cef9dcf33d1d019573
gtalarico <gtalarico.dev@gmail.com>
2020-05-25 23:17:53 -0700

    Cleanup

--- main.tf
+++ main.tf
@@ -1,36 +1,11 @@
 // Provider
+
 provider "aws" {
   version = "2.33.0"
   region = var.aws_region
 }
 
 
-// Templates
-data "template_file" "rolepolicy" {
-   template = file("policies/rolepolicy.json")
-   # Who Can Assume
-   vars = {
-      svc_user = aws_iam_user.user.arn
-   }
-}
-
-data "template_file" "bucketpolicy" {
-   template = file("policies/bucketpolicy.json")
-   vars = {
-     bucket_arn = aws_s3_bucket.bucket.arn
-     svc_user = aws_iam_user.user.arn
-   }
-}
-
-
-resource "aws_iam_user" "lb" {
-  name = "loadbalancer"
-  path = "/system/"
-
-  tags = {
-    tag-key = "tag-value"
-  }
-}
 
 // Resources
 
@@ -47,16 +22,20 @@ resource "aws_s3_bucket" "bucket" {
   }
 }
 
-resource "aws_s3_bucket_policy" "bucket_policy" {
-  bucket = aws_s3_bucket.bucket.id
-  policy = data.template_file.bucketpolicy.rendered
-}
-
-# resource "aws_iam_role" "role" {
-#   name = "${var.project_name}-svc-role"
-#   assume_role_policy = data.template_file.rolepolicy.rendered
-# }
 
 resource "aws_iam_user" "user" {
   name = "${var.project_name}-svc-user"
+}
+
+data "template_file" "bucketpolicy" {
+   template = file("policies/bucketpolicy.json")
+   vars = {
+     bucket_arn = aws_s3_bucket.bucket.arn
+     svc_user = aws_iam_user.user.arn
+   }
+}
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.bucket.id
+  policy = data.template_file.bucketpolicy.rendered
 }
